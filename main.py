7c17874702248ff4d559b88d15a57f8d5593ce4f
Xcelled <j12@ketelaar.com>
2016-12-03 03:37:09 -0500

    Add logging

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
+
+import logging
+import Colorer
+logging.basicConfig(level=logging.WARN)
+log = logging.getLogger(__name__)
+
 import sys
 from PyQt5.QtWidgets import QApplication
 import screenshot
