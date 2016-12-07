999bb96b374e0ae20b550bd067937b8cbe89e701
Scott Ketelaar <git@scottketelaar.com>
2016-12-07 05:49:56 -0500

    Update installer

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ fi
 apt=`which apt-get`
 yum=`which yum`
 pac=`which pacman`
+git=`which git`
 
 pip=`which pip3 || which pip`
 python=`which python3 || which python`
@@ -26,12 +27,17 @@ if [ `expr match "$($python --version)" ".*ython 3"` == "0" ]; then
 	exit 1
 fi
 
+if [ -n "$git" ]; then
+	echo "git is required to install Cap'n Snap"
+	exit 1
+fi
+
 # Try to install repo packages.
 if [ -n "$apt" ]; then
 	# This is for Ubuntu. If debian's different then well...
 	# TODO: If we already have PyQt from pip, no need to get it again.
-	$apt install qtbase5-private-dev python3-pyqt5 pyqt5-dev # libqxt-dev ??
-	# TODO: If pygt5 package not found, try pip?
+	$apt install qtbase5-private-dev python3-pyqt5 pyqt5-dev libxcb-render0-dev libffi-dev # libqxt-dev ??
+	# TODO: If pyqt5 package not found, try pip?
 elif [ -n "$yum" ]; then
 	echo "yum is not currently supported"
 	$(exit 1)
@@ -41,7 +47,7 @@ elif [ -n "$pac" ]; then
 else
 	echo "cannot determine package manager"
 	$(exit 1)
-#endif
+fi
 
 found=1
 if [ $? -ne 0 ]; then
@@ -49,10 +55,10 @@ if [ $? -ne 0 ]; then
 	found=
 fi
 
-# Install python-xlib (linux specific)
-$pip install python-xlib
+# Install xcffib (linux specific)
+$pip install xcffib
 if [ $? -ne 0 ]; then
-	echo "ERROR: Could not install python-xlib"
+	echo "ERROR: Could not install xcffib"
 	exit 2
 fi
 
@@ -62,6 +68,22 @@ if [ $? -ne 0 ]; then
 	echo "ERROR: Could not install necessary packages."
 	exit 2
 fi
+
+# Install xpybutil
+rm -rf xpybutil
+$git clone https://github.com/BurntSushi/xpybutil.git
+if [ $? -ne 0 ]; then
+	echo "ERROR: Could not clone xpybutil"
+	exit 2
+fi
+cd xpybutil
+$python setup.py install
+if [ $? -ne 0 ]; then
+	echo "ERROR: Could not install xpybutil."
+	exit 2
+fi
+cd ..
+rm -rf xpybutil
 
 # TODO: Install this.
 
