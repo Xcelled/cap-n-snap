2fd443b1d44be73f1f590e669fe7783d0d1aa445
Megan Ketelaar <git@meganketelaar.com>
2016-12-07 07:37:43 -0500

    Touchup on hotkeys

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ import Colorer
 logging.basicConfig(level=logging.WARN)
 log = logging.getLogger(__name__)
 
+if DEBUG: logging.getLogger().setLevel(logging.DEBUG)
+
+
 import sys, os, plat
 from PyQt5.QtGui import QKeySequence
 from PyQt5.QtWidgets import QApplication
 
 log.info('Detected OS as {}'.format('Linux' if plat.LINUX else ('Windows' if plat.Windows else 'Mac')))
 
-if plat.Supports.Hotkeys: from system_hotkey import SystemHotkey
+if plat.Supports.Hotkeys: import hotkeys
 
 # For easier usage calculate the path relative to here.
 here = os.path.abspath(os.path.dirname(__file__))
@@ -23,8 +26,7 @@ app = QApplication(sys.argv)
 
 # Add kill-switch for development testing
 if DEBUG and plat.Supports.Hotkeys:
-	kill = SystemHotkey()
-	kill.register(('control', 'shift', 'k'), callback=lambda e:app.exit())
+	hotkeys.default.register('ctrl+shift+k', lambda e:app.exit())
 #endif
 
 app.exec_()
